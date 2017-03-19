44b9a4b4e5f632e98cd4e59042149595fd78ff2f
Guillem Cruz <gcquiroga@gmail.com>
2017-03-19 01:41:08 +0100

    Logging afegit a les rutes de totem

--- routes/totems.js
+++ routes/totems.js
@@ -1,21 +1,30 @@
+var l = require('debug')('etorn:routes:totems');
+
 //Totem Routes
 module.exports = function(router) {
   var Totem = require('../models/Totem');
 
   router.route('/totems')
     .post(function(req, res) {
+      l('New totem registration');
       var totem = new Totem();
       totem.save(function(err, t) {
-        if (err)
+        if (err) {
+          l('Totem save failed: %s', err);
           return res.send(err);
+        }
 
+        l('Totem sucessfully saved (%s)', t.id);
         res.json({ message: 'Totem created!', totemId: t.id});
       });
     })
     .get(function(req, res) {
+      l('GET /totems (get list of totems)')
       Totem.find(function(err, totem) {
-        if (err)
+        if (err) {
+          l('Totem find failed: %s', err);
           return res.send(err);
+        }
 
         res.json(totem);
       });
@@ -23,20 +32,27 @@ module.exports = function(router) {
 
   router.route('/totems/:totem_id')
     .get(function(req, res) {
+      l('GET /totems/%s', req.params.totem_id);
       Totem.findById(req.params.totem_id, function(err, totem) {
-        if (err)
+        if (err) {
+          l('Totem not found (%s): %s', req.params.totem_id, err);
           return res.send(err);
+        }
 
         res.json(totem);
       });
     })
     .delete(function(req, res) {
+      l('DELETE /totems/%s', req.params.totem_id)
       Totem.remove({
         _id: req.params.totemid
       }, function(err, totem) {
-        if (err)
+        if (err) {
+          l('Totem removal failed (%s): %s', req.params.totem_id, err)
           return res.send(err);
+        }
 
+        l('Totem successfully removed (%s)', req.params.totem_id);
         res.json({ message: 'Successfully deleted' });
       });
     });
